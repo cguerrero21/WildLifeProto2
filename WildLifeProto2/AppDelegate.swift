3d0b4957f2eaa3cc2cee6bf86df2d426acdb2008
montzerrath <montzerrathchavez4@gmail.com>
2019-08-01 09:03:21 -0500

    Updated version of wildlife

--- WildLifeProto2/AppDelegate.swift
+++ WildLifeProto2/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Change color of tab bar
+        UITabBar.appearance().tintColor = UIColor.green
+        
         return true
     }
 
---
+++ WildLifeProto2/SigninViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignInViewController.swift
+//  WildLifeProto2
+//
+//  Created by SER-YCL19 on 7/24/19.
+//  Copyright © 2019 CarlosG. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+
+
+}
